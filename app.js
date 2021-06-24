6c1617b973ae263420e8287b3f6c1958decadf64
bootspace <gregudogu@gmail.com>
2021-06-24 01:37:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser : true,
+  useUnifiedTopology : true
+});
+//Setting view engines
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended : false}));
+
+//Home route
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls : shortUrls});
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect('/');
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl});
+
+  if(shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+})
+
+//Setting up server
+app.listen(process.env.PORT || 2000, () => {
+  console.log("Server running on Port 2000");
+});
